1d4a3e1802dddbb6143ff5f9c24338ea4507acfa
Andrey Toropchin <4ndrey@gmail.com>
2016-01-28 11:11:52 +0500

    adding request descriptor for request with body

--- strip.sh
+++ strip.sh
@@ -3,7 +3,8 @@ files="../../FMCG/Networking/*.m"
 for i in $files
 do
     sed '/^    $/d;/^$/{N;/^\n$/d;}' $i > $i.out
-    mv $i.out $i
+    sed 's/* requestDescriptor/ requestDescriptor/' $i.out > $i
+    rm $i.out
 done
 
 sed -e 's/NSString/String/' -e 's/NSNumber/Integer 32/' -e 's/NSArray/Transformable/' ../../FMCG/Models/CoreData.xcdatamodel/contents > out
