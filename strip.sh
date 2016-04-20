65e90119f4d528c1ed7c2d026cab2e05a84e1013
Andrey Toropchin <4ndrey@gmail.com>
2016-04-20 15:00:03 +0500

    added indexies for db

--- strip.sh
+++ strip.sh
@@ -7,5 +7,5 @@ do
     rm $i.out
 done
 
-sed -e 's/NSString/String/' -e 's/NSNumber/Integer 32/' -e 's/NSArray/Transformable/' ../../FMCG/Models/CoreData.xcdatamodel/contents > out
+sed -e 's/NSString/String/' -e 's/NSNumber/Integer 32/' -e 's/NSArray/Transformable/' -e 's/id"/id" indexed="YES"/' -e 's/Id"/Id" indexed="YES"/' -e 's/title"/title" indexed="YES"/' ../../FMCG/Models/CoreData.xcdatamodel/contents > out
 mv out ../../FMCG/Models/CoreData.xcdatamodel/contents
